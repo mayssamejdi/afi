b220238072e7c448b767b78c0074e40b90b89b74
Mayssa Mejdi <mayssa.mejdi@inetum.com>
2022-09-01 16:43:02 +0200

    first commit

---
+++ src/main/java/com/example/afi/AfiApplication.java
@@ -0,0 +1,13 @@
+package com.example.afi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfiApplication.class, args);
+    }
+
+}
